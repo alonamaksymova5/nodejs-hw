7b92f4cae09d6e367cf5e8887c162c2b9be7cbe6
Alyona Maksymova <alonamaksymova5@gmail.com>
2025-12-03 22:20:45 +0200

    did homework

--- src/server.js
+++ src/server.js
@@ -1,67 +1,39 @@
-import 'dotenv/config';
 import express from 'express';
-import pino from 'pino-http';
+import 'dotenv/config';
 import cors from 'cors';
 
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+
+import notesRoutes from './routes/notesRoutes.js';
+
 const app = express();
-
-app.use(express.json());
-app.use(cors());
-
 const PORT = process.env.PORT ?? 3000;
 
+app.use(logger);
 app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
+  express.json({ type: ['application/json', 'application/vnd.api + json'] }),
 );
+app.use(cors());
 
-// маршрут, що повертає всі нотатки
+app.use(notesRoutes);
 
 app.get('/notes', (req, res) => {
   res.status(200).json({ message: 'Retrieved all notes' });
 });
-
-// маршрут, що повертає нотатку за її id
 
 app.get('/notes/:noteId', (req, res) => {
   const { noteId } = req.params;
   res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
 });
 
-//маршрут для імітації виникнення помилки
+app.use(notFoundHandler);
 
-app.get('/test-error', (req, res, next) => {
-  next(new Error('Simulated server error'));
-});
+app.use(errorHandler);
 
-//middleware для неіснуючих маршрутів
-
-app.use((req, res, next) => {
-  res.status(404).json({ message: 'Route not found' });
-});
-
-// middleware для обробки помилок
-
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: 'Internal Server Error',
-    error: err.message,
-  });
-});
-
-//слухаємо порт
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
