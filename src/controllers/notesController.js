4c4518ceb3df6d341ae01859faafc30c7dde0d4e
Alyona Maksymova <alonamaksymova5@gmail.com>
2025-12-11 17:03:11 +0200

    delete promise

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,6 +1,5 @@
 import createHttpError from 'http-errors';
 import { Note } from '../models/note.js';
-import { Promise } from 'mongoose';
 
 export const getAllNotes = async (req, res) => {
   const page = Number(req.query.page) || 1;
